4250748c12f1b948c4a14aba8c85ed944909f330
Satyajeet Desale <Satyajeet@Satyajeets-Mac-mini.local>
2020-08-05 13:41:50 +0530

    Updates

---
+++ precache-manifest.852fc95ce928056749304e4fb8f74be4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e59d8c08cd4de4657d446e4fd1e93a",
+    "url": "/developer-profile/index.html"
+  },
+  {
+    "revision": "3121c37c50b4fa66ba56",
+    "url": "/developer-profile/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2317d118e889e96299c7",
+    "url": "/developer-profile/static/js/2.d472ffca.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/developer-profile/static/js/2.d472ffca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3121c37c50b4fa66ba56",
+    "url": "/developer-profile/static/js/main.4150b25b.chunk.js"
+  },
+  {
+    "revision": "6036c675039106be951e",
+    "url": "/developer-profile/static/js/runtime-main.929df1da.js"
+  }
+]);
